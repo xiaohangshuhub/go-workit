b5acc2d3d256fe52ccaf127d79bf1dd122fed6f6
zhangxiancheng <zhangxiancheng@sinsegye.com.cn>
2025-09-10 14:38:10 +0800

    feat: webapi 添加response

--- internal/service1/webapi/hello.go
+++ internal/service1/webapi/hello.go
@@ -2,7 +2,7 @@ package webapi
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/xiaohangshuhub/go-workit/pkg/api"
+	"github.com/xiaohangshuhub/go-workit/internal/service1/webapi/response"
 	"go.uber.org/zap"
 )
 
@@ -30,7 +30,7 @@ func Hello(
 // @Security BearerAuth
 func helloNewb(log *zap.Logger) gin.HandlerFunc {
 	return func(c *gin.Context) {
-		data := api.Success("你好,小航书")
+		data := response.Success("你好,小航书")
 		c.JSON(200, data)
 	}
 }
--- internal/service1/webapi/response/response.go
+++ internal/service1/webapi/response/response.go
@@ -1,4 +1,4 @@
-package api
+package response
 
 const (
 	// 成功
