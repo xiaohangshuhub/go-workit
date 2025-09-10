5e0be50a45f2cf8a318d04ed9d1f96f97fd2d2a2
zhangxiancheng <zhangxiancheng@sinsegye.com.cn>
2025-09-10 14:44:08 +0800

    feat: update workit pkg

--- internal/service1/webapi/hello.go
+++ internal/service1/webapi/hello.go
@@ -1,8 +1,9 @@
 package webapi
 
 import (
+	"cli-echo/internal/service1/webapi/response"
+
 	"github.com/labstack/echo/v4"
-	"github.com/xiaohangshuhub/go-workit/pkg/api"
 	"go.uber.org/zap"
 )
 
@@ -29,7 +30,7 @@ func Hello(
 // @Router /hello [get]
 func HelloNewb(log *zap.Logger) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		data := api.Success("你好,小航书")
+		data := response.Success("你好,小航书")
 		return c.JSON(200, data)
 	}
 }
